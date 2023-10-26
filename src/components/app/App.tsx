ca327dbdddba4e4b98a080a5cffd7e8cfe0787ec
tzchrzanowski <tzchrzanowski@gmail.com>
2023-10-26 10:38:52 +0800

    added tailwind and applied styling to header

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,9 +6,15 @@ import logoCaption from 'assets/logo-caption.svg';
 function App() {
   return (
     <div className={"App"}>
-      <div className={"App-header"}>
-          <img src={logoIcon} alt={"logo"} />
-          <img src={logoCaption} alt={"natur.ally"} />
+      <div className={"App-header flex flex-row items-center justify-start"}>
+          <div className={"ml-10 my-6 flex flex-row"}>
+              <img className={"mr-2"} src={logoIcon} alt={"logo"} />
+              <div className={"flex items-end"}>
+                  <img src={logoCaption} alt={"natur.ally"} />
+              </div>
+          </div>
+      </div>
+      <div className={""}>
         App
       </div>
     </div>
