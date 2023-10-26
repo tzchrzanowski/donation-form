233bb4daa7d41fa4e48a2692247c4e4617355c83
tzchrzanowski <tzchrzanowski@gmail.com>
2023-10-26 11:15:21 +0800

    moved TopHeader navigation to separate component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-import logoIcon from 'assets/logo-icon.svg';
-import logoCaption from 'assets/logo-caption.svg';
+import TopHeader from "../top-header/TopHeader";
 
 function App() {
   return (
     <div className={"App"}>
-      <div className={"App-header flex flex-row items-center justify-start"}>
-          <div className={"ml-10 my-6 flex flex-row"}>
-              <img className={"mr-2"} src={logoIcon} alt={"logo"} />
-              <div className={"flex items-end"}>
-                  <img src={logoCaption} alt={"natur.ally"} />
-              </div>
-          </div>
-      </div>
+      <TopHeader />
       <div className={""}>
         App
       </div>
