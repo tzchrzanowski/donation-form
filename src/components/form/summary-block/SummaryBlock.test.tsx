632bfa96ad667a0a41a22e0edc7c7b7772ca4eb0
tzchrzanowski <tzchrzanowski@gmail.com>
2023-10-29 16:32:56 +0800

    added tests to summary block

---
+++ src/components/form/summary-block/SummaryBlock.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import FormControls from './SummaryBlock';
+import {dateMonthAndYear} from "utils/dateHelpers";
+
+describe('Summary block tests:', () => {
+    const dateObjectMock: dateMonthAndYear = {
+        amountOfMonths: 2,
+        month: "January",
+        monthNumber: 1,
+        year: 2024,
+    }
+
+    test('test that total value in rendered sumamry block is multiplied by two', () => {
+        render(<FormControls dateObject={dateObjectMock} donationPerMonthAmount={400}/>);
+        const totalAmount = screen.getByTestId('total-value-id');
+
+        expect(totalAmount).toHaveTextContent("$800.00");
+    });
+
+    test('test that checks that every month amount is displayed as intended', () => {
+        render(<FormControls dateObject={dateObjectMock} donationPerMonthAmount={400}/>);
+        const everyMonthAmount = screen.getByTestId('every-month-amount-id');
+
+        expect(everyMonthAmount).toHaveTextContent("$ 400.00");
+    });
+
+    test('test that checks that date is displayed as intended', () => {
+        render(<FormControls dateObject={dateObjectMock} donationPerMonthAmount={400}/>);
+        const dateComponent = screen.getByTestId('date-id');
+
+        expect(dateComponent).toHaveTextContent("January 2024");
+    });
+});
--- src/components/form/summary-block/SummaryBlock.tsx
+++ src/components/form/summary-block/SummaryBlock.tsx
@@ -20,13 +20,13 @@ const SummaryBlock: React.FC<summaryBlockProps> = ({donationPerMonthAmount, date
     return (<div data-testid={"form-summary"}>
         <div className={"flex flex-row justify-between items-center mt-8 mx-4" }>
             <span className={"pl-4 mr-2 summary-caption"}>Total amount</span>
-            <span className={"summary-amount"}>${formattedTotalValue}</span>
+            <span className={"summary-amount"} data-testid={"total-value-id"}>${formattedTotalValue}</span>
         </div>
         <div className={"description-caption px-4 py-5 mt-6"}>
             <span className={"description-caption"}>{t("you-will-send")}</span>
-            <span className={"value-caption"}>$ {formattedDonationsPerMonth}</span>
+            <span className={"value-caption"} data-testid={"every-month-amount-id"}>$ {formattedDonationsPerMonth}</span>
             <span className={"description-caption"}>{t('every-month')}</span>
-            <span className={"value-caption"}>{dateObject.month} {dateObject.year}</span>
+            <span className={"value-caption"} data-testid={"date-id"}>{dateObject.month} {dateObject.year}</span>
             <span className={"description-caption"}>{t('thank-you')}</span>
         </div>
     </div>)
